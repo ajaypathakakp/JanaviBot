57078f5b65af608fbadf9953d4a7ee8bcdd863fe
ajaypathakakp <49590931+ajaypathakakp@users.noreply.github.com>
2020-10-10 06:13:39 +0000

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hey {}, my name is {}! My name is Rucchi aka Bittu. I manage various Cybersecurity groups all over the Telegram.
+Hey {}, my name is {}! aka Bittu. I manage various Cybersecurity groups all over the Telegram.
 If you have any questions on how to use me please input /help
 
 This bot is managed by Ajay Pathak and Riddhi Tiwari.
